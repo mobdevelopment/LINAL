8ab611cf65d55b49b0ae3a6d8ff065edee1620c0
Mark van Kleef <mkleef@avans.nl>
2017-04-01 20:58:39 +0200

    Initial setup

---
+++ LINAL/Controller/MainController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LINAL.view;
+
+namespace LINAL.Controller
+{
+    public class MainController
+    {
+        private MainWindow view;
+
+        public MainController(MainWindow view)
+        {
+            this.view = view;
+        }
+    }
+}
---
+++ LINAL/Model/Square.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINAL.Model
+{
+    public class Square : Shape
+    {
+        public void draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
