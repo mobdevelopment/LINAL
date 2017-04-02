3e456c653cc75c57f878324e7756e09714a9bdc8
Michael Schouten <manschouten@hotmail.com>
2017-04-03 00:58:59 +0200

    matrix rotation method done

--- LINAL/Model/Calc/Matrix.cs
+++ LINAL/Model/Calc/Matrix.cs
@@ -44,7 +44,7 @@ namespace LINAL.Model.Calc
         - Matrix Scalar
         - Matrix Translate
         - Rotate
-        ...
+        ... 
         */
 
         /* translates degrees to radians */
@@ -74,8 +74,51 @@ namespace LINAL.Model.Calc
         {
             // convert degrees to radians
             var radians = degreesToRadians(degrees);
+            var tOne = Math.Atan2(v.z, v.x);
+            var tTwo = Math.Atan2(v.y, Math.Sqrt(Math.Pow(v.x, 2) + Math.Pow(v.z, 2)));
 
-            //
+            // Terug naar de ruimte en startpositie
+            Matrix m7 = translateMatrix(v.x, v.y, v.z);
+            Matrix m6 = new Matrix(new double[4, 4] {
+                { Math.Cos(tOne), 0, -Math.Sin(tOne), 0 },
+                { 0, 1, 0, 0  },
+                { Math.Sin(tOne), 0, Math.Cos(tOne), 0 },
+                { 0, 0, 0, 1 }
+            });
+            // Rotatie-as naar de x-as, roteren om de x-as en terug naar xy-vlak
+            Matrix m5 = new Matrix(new double[4, 4] {
+                { Math.Cos(tTwo), -Math.Sin(tTwo), 0, 0 },
+                { Math.Sin(tTwo),  Math.Cos(tTwo), 0, 0 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 1 }
+            });
+            Matrix m4 = new Matrix(new double[4, 4] {
+                { 1, 0, 0, 0 },
+                { 0, Math.Cos(radians), -Math.Sin(radians), 0 },
+                { 0, -Math.Sin(radians), Math.Cos(radians), 0 },
+                { 0, 0, 0, 1 }
+            });
+            Matrix m3 = new Matrix(new double[4, 4] {
+                { Math.Cos(tTwo), Math.Sin(tTwo), 0, 0 },
+                { -Math.Sin(tTwo), Math.Cos(tTwo), 0, 0 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 1 }
+            });
+
+            // Breng de rotatie as door O en naar xy-vlak
+            Matrix m2 = new Matrix(new double[4, 4] {
+                { Math.Cos(tOne), 0, Math.Sin(tOne), 0 },
+                { 0, 1, 0, 0 },
+                { -Math.Sin(tOne), 0, Math.Cos(tOne), 0 },
+                { 0, 0, 0, 1 }
+            });
+            Matrix m1 = new Matrix(new double[4, 4] {
+                { 1, 0, 0, (v.x * -1) },
+                { 0, 1, 0, (v.y * -1) },
+                { 0, 0, 1, (v.z * -1) },
+                { 0, 0, 0, 1 }
+            });
+            return m7 * (m6 * (m5 * (m4 * (m3 * (m2 * m1)))));
         }
     }
 }
